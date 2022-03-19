161752c21ff40cbeb31cb638762fc4ee8cf6216a
Yarnell Yarnell <yarnnyarmy@gmail.com>
2022-03-19 19:37:55 -0400

    Saving the first fractal in an array and the second fractal into an array

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import MetaTrader5 as mt5
+from scipy.special import logsumexp
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
@@ -79,18 +80,18 @@ def uptrend(df1, n2, n1):
 
 
 def uptrend2(df2, n3, n4):
-    for a in range(n3, n4 - 3):
-        if df2.high[a] > frac1[0][1]:
+    for j in range(n3, n4 - 3):
+        if df2.high[j] > frac1[0][1]:
             break
-        if df2.time[a] == frac1[0][2]:
+        if df2.time[j] == frac1[0][2]:
             break
-        if df2.high[a] < df2.high[a + 1]:
+        if df2.high[j] < df2.high[j + 1]:
             break
-        if df2.high[a] < df2.high[a + 2]:
+        if df2.high[j] < df2.high[j + 2]:
             break
-        if df2.high[a] < df2.high[a - 1]:
+        if df2.high[j] < df2.high[j - 1]:
             break
-        if df2.high[a] < df2.high[a - 2]:
+        if df2.high[j] < df2.high[j - 2]:
             break
         return True
     return False
@@ -117,63 +118,87 @@ e = 1000
 # add the row number, the high price of the bar, and the time of the bar
 
 
-for row in range(n1, e + n1):
-    if uptrend(df, row, e):
-        if row < e - n2:
-            frac1.append((row, df.high[row], df.time[row]))
+def find_first_frac(first_row):
+    for firstRow in range(first_row, e + n1):
+        if uptrend(df, firstRow, e):
+            if firstRow < e - n2:
+                frac1.append((firstRow, df.high[firstRow], df.time[firstRow]))
+            break
 
 # for loop to add elements to the second list that are lower than the first fractal
-for row in range(n1, e + n1):
-    if uptrend2(df, row, e):
-        if row < e - n2:
-            frac2.append((row, df.high[row], df.time[row]))
+
+
+def find_sec_frac():
+    for secondRow in range(frac1[0][0], e + n1):
+        if uptrend2(df, secondRow, e):
+            if secondRow < e - n2:
+                frac2.append((secondRow, df.high[secondRow], df.time[secondRow]))
+
+
 print("The fractals for 2 are the first time are", len(frac2))
 print(frac2)
 # a for loop to see if any high from a candle breaks the trend line
 # if it does then remove that fractal from the list and calculate the next one
 
 # for loop from the first frac to the second frac
+a = 3
+downSlope1 = []
+downSlope2 = []
+find_first_frac(a)
+find_sec_frac()
 
-downSlope = []
-a = 0
-b = 0
 c = 0
-while c < len(frac2):
-    for bar in range(frac1[0][a], frac2[c][0]):
+
+while len(downSlope2) < 1:
+
+    for bar in range(frac1[0][0], frac2[c][0] + 1):
 
         # df1 = pd.DataFrame(bar)
         # The slope of the first frac[high] - frac2[high] / frac[bar] - frac2[bar]- sec frac[bar]
         barCount = frac2[c][0]
+        barCount2 = frac2[c][0] + 1
         price1 = frac1[0][1] - frac2[c][1]
         bar1 = frac1[0][0] - frac2[c][0]
-        totalSlope1 = price1 / bar1
-        price2 = df.high[bar] - frac2[c][1]
-        bar2 = bar - frac2[c][0]
+        totalSlope1 = round(price1 / bar1, 5)
+        priceSecondBar = frac2[c][1]
+        highOfDf = df.high[bar]
+        barSecondBar = frac2[c][0]
+        price2 = highOfDf - priceSecondBar
+        bar2 = bar - barSecondBar
         totalSlope2 = price2 / bar2
-        if len(frac2) == 1 and totalSlope2 < totalSlope1:
-            a += 1
-            break
-        if totalSlope2 < totalSlope1:
+
+        if bar < barCount and totalSlope2 < totalSlope1 and c < (len(frac2) - 1):
             c += 1
             break
-        # The slope of bar high to the second frac
-        # slope2 = df.high[bar] - frac2[c][1] // abs(bar - frac2[c][0])
-        # inner for loop for calculating each bar to the second frac
-        # if the slope of slope2 is larger than slope one then break out the loop
-        # and delete the item from the list
-
-        if bar == barCount+1 and totalSlope2 >= totalSlope1:
-            downSlope.append(frac2[c])
-
-    c += 1
-
+        if bar == (barCount - 1) and totalSlope2 > totalSlope1:
+            if len(downSlope1) > 0:
+                downSlope1.clear()
+            downSlope1.append(frac1)
+            downSlope2.append(frac2[c])
+            print("Down slope 1 is ", downSlope1)
+            print("Down slope 2 is ", downSlope2)
+            c += 1
+            break
+        if c == (len(frac2) - 1) and totalSlope2 < totalSlope1:
+            b = frac1[0][0] + 1
+            c = 0
+            frac1.clear()
+            frac2.clear()
+            find_first_frac(b)
+            find_sec_frac()
+            break
+            # The slope of bar high to the second frac
+            # slope2 = df.high[bar] - frac2[c][1] // abs(bar - frac2[c][0])
+            # inner for loop for calculating each bar to the second frac
+            # if the slope of slope2 is larger than slope one then break out the loop
+            # and delete the item from the list
 
 # setting up a new frac
 
 # print the length of the list and all the elements
 
 
-print("The fractals for 1 are ", len(frac1))
-print(frac1)
-print("The fractals for 2 are ", len(frac2))
-print(frac2)
+print("The first slope is ", downSlope1)
+#print(frac1)
+print("The second slope length is ", len(downSlope2))
+#print(frac2)
